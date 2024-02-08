bd7da9857a01a8b7d89d301e08e4a4d94d18529e
IgaJ <i.jarzewicz@gmail.com>
2024-02-08 16:22:26 +0100

    non-standard mapper to map animalId in Action when convert to ActionDTO

--- src/main/java/com/example/shelter/mappers/ActionMapper.java
+++ src/main/java/com/example/shelter/mappers/ActionMapper.java
@@ -1,11 +1,18 @@
 package com.example.shelter.mappers;
 
 import com.example.shelter.dto.ActionDTO;
+import com.example.shelter.dto.AnimalDTO;
 import com.example.shelter.entity.Action;
+import com.example.shelter.entity.Animal;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(uses = AnimalMapper.class)
 public interface ActionMapper {
     Action toAction (ActionDTO actionDTO);
+    //ActionDTO toActionDTO (Action action);
+
+    @Mapping(target = "animalId", source = "animal.id") // target to pole w DTO a source to pole w animal
     ActionDTO toActionDTO (Action action);
+
 }
