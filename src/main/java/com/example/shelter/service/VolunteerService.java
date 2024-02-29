f9bb77e9cce1ff53bdedd93272ae3b2b0d72165a
IgaJ <i.jarzewicz@gmail.com>
2024-02-29 12:00:49 +0100

    service interfaces deleted

--- src/main/java/com/example/shelter/service/VolunteerService.java
+++ src/main/java/com/example/shelter/service/VolunteerService.java
@@ -1,4 +1,14 @@
 package com.example.shelter.service;
 
-public interface VolunteerService {
+import com.example.shelter.mappers.VolunteerMapper;
+import com.example.shelter.repository.VolunteerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VolunteerService {
+
+    private final VolunteerRepository volunteerRepository;
+    private final VolunteerMapper volunteerMapper;
 }
