f13da1953d854352c7ceb5366fdb5fc92ab5d6a8
IgaJ <i.jarzewicz@gmail.com>
2024-02-29 12:00:49 +0100

    service interfaces deleted

--- src/main/java/com/example/shelter/service/AnimalService.java
+++ src/main/java/com/example/shelter/service/AnimalService.java
@@ -1,36 +1,169 @@
 package com.example.shelter.service;
 
 import com.example.shelter.dto.AnimalDTO;
+import com.example.shelter.entity.Animal;
+import com.example.shelter.entity.Box;
+import com.example.shelter.mappers.AnimalMapper;
+import com.example.shelter.repository.AnimalRepository;
+import com.example.shelter.repository.BoxRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface AnimalService {
+@Service
+@RequiredArgsConstructor
+public class AnimalService {
+    private final AnimalRepository animalRepository;
+    private final AnimalMapper animalMapper;
+    private final BoxService boxService;
+    private final BoxRepository boxRepository;
 
-    AnimalDTO saveNewAnimal(AnimalDTO animal);
+    @Transactional
+    public AnimalDTO saveNewAnimal(AnimalDTO animalDTO) { //
+        Animal newAnimal = animalMapper.animalDTOToAnimal(animalDTO);
+        animalRepository.save(newAnimal);
+        boxService.addAnimal(newAnimal);
+        return animalMapper.animalToAnimalDTO(animalRepository.save(newAnimal));
+    }
 
-    void vaccinate(Integer id);
-    void walk(Integer id);
-    void adopt(Integer id);
+    public void vaccinate(Integer id) {
+        Animal animal = animalRepository.findById(id).orElseThrow(() -> new AnimalServiceException("Nie ma takiego zwierzęcia"));
+        animal.setVaccinated(true);
+        animalRepository.save(animal);
+    }
 
-    List<AnimalDTO> listAnimals();
+    public void walk(Integer id) {
+        Animal animal = animalRepository.findById(id).orElseThrow(() -> new AnimalServiceException("Nie ma takiego zwierzęcia"));
+        animal.setLastWalkDate(LocalDate.now());
+        animalRepository.save(animal);
+    }
 
-    List<AnimalDTO> getAnimalByName(String name);
+    public void adopt(Integer id) {
+        Animal animal = animalRepository.findById(id).orElseThrow(() -> new AnimalServiceException("Nie ma takiego zwierzęcia"));
+        animal.setAdopted(true);
+        animal.setAdoptionDate(LocalDate.now());
+        animalRepository.save(animal);
+    }
 
-    List<AnimalDTO> getByAge(Integer age);
 
-    List<AnimalDTO> getBySex(String sex);
+    public List<AnimalDTO> listAnimals() {
+        return animalRepository.findAll()
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
 
-    List<AnimalDTO> getAnimalsBySize(String size);
+    public List<AnimalDTO> listNonVaccinated() {
+        return animalRepository.getNonVaccinated(false)
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
 
-    Optional<AnimalDTO> getAnimalById(Integer id);
+    public List<AnimalDTO> listAvailableForAdoption() {
+        return animalRepository.getAvailableForAdoption()
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
 
-    void deleteById(Integer id);
+    public List<AnimalDTO> listAvailable(Boolean vaccinated, Boolean adopted) {
+        return animalRepository.findAllByVaccinatedAndAdopted(vaccinated, adopted)
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
 
-    Optional<AnimalDTO> patchAnimalById(Integer animalId, AnimalDTO animalDTO);
 
-    List<AnimalDTO> listNonVaccinated();
+    public List<AnimalDTO> getAnimalByName(String name) {
+        return animalRepository.getAnimalByName(name)
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
 
-    List<AnimalDTO> listAvailableForAdoption();
+
+    public List<AnimalDTO> getByAge(Integer age) {
+        return animalRepository.getAnimalByAge(age)
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
+
+
+    public List<AnimalDTO> getBySex(String sex) {
+        return animalRepository.getAnimalBySex(sex)
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
+
+
+    public List<AnimalDTO> getAnimalsBySize(String size) {
+        return animalRepository.getAnimalBySize(size)
+                .stream()
+                .map(animal -> animalMapper.animalToAnimalDTO(animal))
+                .collect(Collectors.toList());
+    }
+
+
+    public Optional<AnimalDTO> getAnimalById(Integer id) {
+        return Optional.ofNullable(animalMapper.animalToAnimalDTO(animalRepository.findById(id).orElseThrow(null)));
+    }
+
+
+    public void deleteById(Integer AnimalId) {
+        Animal foundAnimal = animalRepository.findById(AnimalId).orElse(null);
+        Box box = boxRepository.findBoxByAnimalId(AnimalId);
+        if (box != null) {
+            box.getAnimals().remove(foundAnimal);
+            boxRepository.save(box);
+        }
+        animalRepository.deleteById(AnimalId);
+    }
+
+
+    public Optional<AnimalDTO> patchAnimalById(Integer animalId, AnimalDTO animalDTO) {
+        Animal existing = animalRepository.getAnimalById(animalId);
+        if (StringUtils.hasText(animalDTO.getName())) {
+            existing.setName(animalDTO.getName());
+        }
+        if (StringUtils.hasText(animalDTO.getSex())) {
+            existing.setSex(animalDTO.getSex());
+        }
+        if (StringUtils.hasText(animalDTO.getSize())) {
+            existing.setSize(animalDTO.getSize());
+        }
+        if (animalDTO.getAge() != null) {
+            existing.setAge(animalDTO.getAge());
+        }
+        if (animalDTO.getArrivalDate() != null) {
+            existing.setArrivalDate(animalDTO.getArrivalDate());
+        }
+        if (StringUtils.hasText(animalDTO.getDescription())) {
+            existing.setDescription(animalDTO.getDescription());
+        }
+        if (animalDTO.getAdopted() != null) {
+            existing.setAdopted(animalDTO.getAdopted());
+        }
+        if (animalDTO.getVaccinated() != null) {
+            existing.setVaccinated(animalDTO.getVaccinated());
+        }
+        if (animalDTO.getVaccinationDate() != null) {
+            existing.setVaccinationDate(animalDTO.getVaccinationDate());
+        }
+        if (animalDTO.getAdoptionDate() != null) {
+            existing.setAdoptionDate(animalDTO.getAdoptionDate());
+        }
+        if (animalDTO.getLastWalkDate() != null) {
+            existing.setLastWalkDate(animalDTO.getLastWalkDate());
+        }
+        return Optional.ofNullable(animalMapper.animalToAnimalDTO(existing));
+    }
 }
-
--- src/main/java/com/example/shelter/service/BoxService.java
+++ src/main/java/com/example/shelter/service/BoxService.java
@@ -2,37 +2,170 @@ package com.example.shelter.service;
 
 import com.example.shelter.dto.BoxDTO;
 import com.example.shelter.entity.Animal;
+import com.example.shelter.entity.Box;
+import com.example.shelter.mappers.BoxMapper;
+import com.example.shelter.repository.AnimalRepository;
+import com.example.shelter.repository.BoxRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface BoxService {
+@Service
+@RequiredArgsConstructor
+public class BoxService {
+    private final BoxRepository boxRepository;
+    private final BoxMapper boxMapper;
+    private final AnimalRepository animalRepository;
 
-    BoxDTO getBoxById(Integer id);
+    public void addAnimal(Animal animal) {
+        Optional<Box> selected = findAvailableBoxWithSizeAndQuarantine();
+        if (selected.isEmpty()) {
+            addNewBox(animal, true);
+        } else {
+            selected.get().addAnimal(animal); // .get do wyciągnięcia boksu z optionala
+            boxRepository.save(selected.get());
+        }
+     /*   findAvailableBoxWithSizeAndQuarantine()
+                .map(box ->{
+                    box.addAnimal(animal);
+                  return  boxMapper.toBoxDTO(boxRepository.save(box));
+                }).or(()->{
+                    addNewBox(animal,true);
+                    return null;
+                });*/
+    }
 
-    void addAnimal(Animal animal);
-    void changeBoxToGivenBoxNumber(Integer animalId, Integer boxId);
-    void changeBoxToFirstQuarantineBox(Integer animalId);
-    public void changeBoxToFirstNoQuarantineBox(Integer animalId);
+    public void changeBoxToGivenBoxNumber(Integer animalId, Integer boxId) {
+        Animal animal = animalRepository.getAnimalById(animalId);
+        Box currentBox = animal.getBox();
+        Box box = boxRepository.findByNumber(boxId)
+                .orElseThrow(() -> new BoxServiceException("Błędny numer boksu")); // rozwinąć obsługę przypadku gdy nie ma numeru/nie ma miejsc - lista dostępnych?
+        if (box.getAnimals().size() < box.getMaxAnimals()) {
+            box.addAnimal(animal);
+            currentBox.getAnimals().remove(animal);
+            boxRepository.save(box);
+            boxRepository.save(currentBox);
+            animalRepository.save(animal);
+        } else {
+            throw new BoxServiceException("W boksie nie ma miejsc");
+        }
+    }
 
-    BoxDTO getBoxByAnimalId(Integer animalId);
+    public void changeBoxToFirstQuarantineBox(Integer animalId) {
+        Animal animal = animalRepository.getAnimalById(animalId);
+        Box currentBox = animal.getBox();
+        currentBox.getAnimals().remove(animal);
+        addAnimal(animal);
+        boxRepository.save(currentBox);
+    }
 
-    List<BoxDTO> listBoxes();
+    public void changeBoxToFirstNoQuarantineBox(Integer animalId) {
+        Animal animal = animalRepository.getAnimalById(animalId);
+        Box currentBox = animal.getBox();
+        currentBox.getAnimals().remove(animal);
+        boxRepository.save(currentBox);
+        Optional<Box> newBox = boxRepository.findBoxesWithSizeLessThanBoxCapacityAndQuarantine(false).stream().findFirst();
+        if (newBox.isEmpty()) {
+            addNewBox(animal, false);
+        } else {
+            Box optional = newBox.get();
+            optional.addAnimal(animal);
+            boxRepository.save(optional);
+        }
+    }
 
-    void deleteById(Integer id);
+    Optional<Box> findAvailableBoxWithSizeAndQuarantine() {
+        return boxRepository.findBoxesWithSizeLessThanBoxCapacityAndQuarantine(true)
+                .stream()
+                .findFirst();
+    }
 
-    BoxDTO addNewBox(Animal animal, Boolean isQuarantine);
+    public BoxDTO addNewBox(Boolean isQuarantine) { // only for dataInitializer
+        return boxMapper.toBoxDTO(saveNewBox(isQuarantine));
+    }
 
-    BoxDTO addNewBox(Boolean isQuarantine);
 
-   // public int countAllBoxes();
+    public BoxDTO addNewBox(Animal animal, Boolean isQuarantine) {
+        Box newBox = saveNewBox(isQuarantine);
+        animal.setBox(newBox);
+        newBox.addAnimal(animal);
+        animalRepository.save(animal);
+        boxRepository.save(newBox);
+        return boxMapper.toBoxDTO(newBox);
+    }
 
-    int findHighestBoxNumber();
+    private Box saveNewBox(Boolean isQuarantine) {
+        Box newBox = Box.builder()
+                .isQuarantine(isQuarantine)
+                .boxNumber(findHighestBoxNumber() + 1)
+                .animals(new HashSet<>())
+                .build();
+        return boxRepository.save(newBox);
+    }
 
-    void deleteByNumber(Integer number);
+    public int findHighestBoxNumber() {
+        return boxRepository.giveHighestBoxNumber();
+    }
 
-    BoxDTO findBoxWithSizeLessThanBoxCapacityAndBoxNumber(Integer boxNumber);
+    public void clean(Integer id) {
+        Box box = boxRepository.getReferenceById(id);
+        box.setCleaningDate(LocalDate.now());
+        boxRepository.save(box);
+    }
 
-    List<BoxDTO> findBoxesWithPlace();
+    public BoxDTO getBoxById(Integer id) {
+        return boxMapper.toBoxDTO(boxRepository.findById(id).orElseThrow());
+    }
 
-    void clean(Integer Id);
+    public BoxDTO getBoxByAnimalId(Integer animalId) {
+        return boxMapper.toBoxDTO(boxRepository.findBoxByAnimalId(animalId));
+    }
+
+    public List<BoxDTO> listBoxes() {
+        return boxRepository.findAll()
+                .stream()
+                .map(box -> boxMapper.toBoxDTO(box))
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(Integer id) {
+        Box box = boxRepository.findById(id).orElse(null);
+        if ((box != null) && (box.getAnimals().isEmpty())) {
+            boxRepository.deleteById(id);
+        } else {
+            throw new BoxServiceException("box with animals impossible to delete, move animals first");
+        }
+    }
+
+    @Transactional // sprawia że operacja otoczona jest transakcją inaczej hibernate nie widzi otwartej transakcji
+    public void deleteByNumber(Integer number) {
+        Box box = boxRepository.findByNumber(number).orElse(null);
+        if ((box != null) && (box.getAnimals().isEmpty())) {
+            boxRepository.deleteByNumber(number);
+        } else {
+            if (box == null) {
+                throw new BoxServiceException("Invalid box number");
+            }
+            if (!box.getAnimals().isEmpty()) {
+                throw new BoxServiceException("Animals in box to be deleted");
+            }
+        }
+    }
+
+    public BoxDTO findBoxWithSizeLessThanBoxCapacityAndBoxNumber(Integer boxNumber) {
+        return boxMapper.toBoxDTO(boxRepository.findBoxWithSizeLessThanBoxCapacityAndBoxNumber(boxNumber));
+    }
+
+    public List<BoxDTO> findBoxesWithPlace() {
+        return boxRepository.findBoxesWithNumberOfAnimalsLessThanBoxCapacity()
+                .stream()
+                .map(box -> boxMapper.toBoxDTO(box))
+                .collect(Collectors.toList());
+    }
 }
