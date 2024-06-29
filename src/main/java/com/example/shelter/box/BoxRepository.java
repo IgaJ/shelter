3aaffe791bebac30ceb0ca1b4bda7fd4f77139e0
IgaJ <i.jarzewicz@gmail.com>
2024-06-29 09:27:23 +0200

    native query in repository

--- src/main/java/com/example/shelter/box/BoxRepository.java
+++ src/main/java/com/example/shelter/box/BoxRepository.java
@@ -1,7 +1,6 @@
 package com.example.shelter.box;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,22 +8,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BoxRepository extends JpaRepository <Box, Integer> {
-    @Query("SELECT b FROM Box b WHERE SIZE(b.animals) < b.maxAnimals AND (: isQuarantine IS NULL OR b.isQuarantine = :isQuarantine) ORDER BY b.id ASC")
+    @Query(value = "SELECT b.* FROM Box b LEFT JOIN Animals a ON b.id = a.box_id GROUP BY b.id, b.max_animals, b.is_quarantine HAVING COUNT(a.id) < b.max_animals AND (:isQuarantine IS NULL OR b.is_quarantine = :isQuarantine) ORDER BY b.id ASC LIMIT 1", nativeQuery = true)
     Optional<Box> findFirstAvailable(@Param("isQuarantine") Boolean isQuarantine);
 
-    @Query("SELECT b FROM Box b WHERE b.boxNumber = :boxNumber AND b.isQuarantine = :isQuarantine")
+    @Query(value = "SELECT * FROM Box b WHERE b.box_number = :boxNumber AND b.is_quarantine = :isQuarantine", nativeQuery = true)
     Optional<Box> findByBoxNumberAndIsQuarantine(@Param("boxNumber") Integer boxNumber, @Param("isQuarantine") Boolean isQuarantine);
 
-    @Query("SELECT b FROM Box b JOIN b.animals a WHERE a.id = :animalId") // zapytanie do tabel łączonych
-    Box findByAnimalId(@Param("animalId") Integer id);
-
-    @Query("SELECT b FROM Box b WHERE SIZE(b.animals) < b.maxAnimals")
+    @Query(value = "SELECT b.* FROM Box b LEFT JOIN Animals a ON b.id = a.box_id GROUP BY b.id HAVING COUNT(a.id) < b.max_animals", nativeQuery = true)
     List<Box> findAvailable();
 
-    @Modifying // dodana @modifying bo z @query hibernate oczekuje zapytania select nie delete
-    @Query("DELETE FROM Box b WHERE b.boxNumber = :boxNumber")
-    void deleteByNumber(@Param("boxNumber") Integer boxNumber);
-
-    @Query("SELECT COALESCE(MAX(b.boxNumber), 0) FROM Box b") // COALESCE - zwraca pierwszą niepustą wartość. Jeśli MAX(b.boxNumber) zwróci null (brak rekordów) to coalesce zwróci 0
+    @Query(value = "SELECT COALESCE(MAX(box_number), 0) FROM Box", nativeQuery = true)
     int giveHighestBoxNumber();
 }
